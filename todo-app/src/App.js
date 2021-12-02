aa98d37e6d8bd5be90eab05086c4e93266af3f81
dnjsvltm201311681 <dnjsvltm201311681@gmail.com>
2021-12-03 01:43:13 +0900

    컴포넌트성능-memo,useState업데이트

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -8,43 +8,34 @@ const App = () => {
   const nextId = useRef(0);
 
   //삽입
-  const onInsert = useCallback(
-    (text) => {
-      const todo = {
-        id: nextId.current,
-        text,
-        checked: false,
-      };
-      setTodos(todos.concat(todo));
-      nextId.current += 1;
-    },
-    [todos]
-  );
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    setTodos((todos) => todos.concat(todo));
+    nextId.current += 1;
+  }, []);
 
   //삭제
-  const onRemove = useCallback(
-    (id) => {
-      setTodos(todos.filter((todo) => todo.id !== id));
-    },
-    [todos]
-  );
+  const onRemove = useCallback((id) => {
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  }, []);
 
   //수정
-  const onToggle = useCallback(
-    (id) => {
-      setTodos(
-        todos.map((todo) =>
-          todo.id === id
-            ? {
-                ...todo,
-                checked: !todo.checked,
-              }
-            : todo
-        )
-      );
-    },
-    [todos]
-  );
+  const onToggle = useCallback((id) => {
+    setTodos((todos) =>
+      todos.map((todo) =>
+        todo.id === id
+          ? {
+              ...todo,
+              checked: !todo.checked,
+            }
+          : todo
+      )
+    );
+  }, []);
 
   useEffect(() => {
     const getList = localStorage.getItem("list");
