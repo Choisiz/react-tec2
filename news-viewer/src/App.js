10db0f08da828734335e19057ced74e7c3e73907
Choi kyungyeul <dnjsvltm327@klnet.co.kr>
2021-12-22 17:47:04 +0900

    외부 api 카테고리 만들기

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,10 +1,18 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import axios from "axios";
 import NewsList from "./component/NewsList";
+import Categories from "./component/Categories";
 //axios로 api정보 불러오기
 //98276eb55fe94eeebfe387703b151d89
 const App = () => {
-  return <NewsList />;
+  const [category, setCategory] = useState("all");
+  const onSelect = useCallback((category) => setCategory(category), []);
+  return (
+    <>
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />
+    </>
+  );
 };
 
 export default App;
