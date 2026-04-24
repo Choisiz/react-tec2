85e84acdedf846da459e58825e7399859f1e3177
Choisiz <dnjsvltm201311671@gmail.com>
2026-04-24 15:19:29 +0900

    new v code Splitting

--- todo-app/src/Component/TodoTemplate.js
+++ todo-app/src/Component/TodoTemplate.js
@@ -1,12 +1,15 @@
-import '../TodoTemplate.scss'
-const TodoTemplate = ({children}) => {
-    console.log('c',children)
-    return(
-        <div className="TodoTemplate">
-            <div className="app-title">일정관리</div>
-            <div className="content">{children}</div>
-        </div>
-    )
+import "../TodoTemplate.scss";
+const TodoTemplate = ({ children }) => {
+  const onClick = () => {
+    import("../notify").then((result) => result.default());
+  };
+  return (
+    <div className="TodoTemplate">
+      <p onClick={onClick}>hello click</p>
+      <div className="app-title">일정관리</div>
+      <div className="content">{children}</div>
+    </div>
+  );
 };
 
 export default TodoTemplate;
